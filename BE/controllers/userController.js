e8406888f39c87da14d8f826d8b417c748370f33
DQThuan <qthuan1234@gmail.com>
2025-09-10 09:20:06 +0700

    fix(controller): add http status code

--- BE/controllers/userController.js
+++ BE/controllers/userController.js
@@ -3,48 +3,50 @@ import userModel from '../models/userModel.js';
 import jwt from "jsonwebtoken"
 import bcrypt from 'bcrypt'
 
-const createToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET)
-}
-
 const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body
 
         const user = await userModel.findOne({ email })
         if (!user) {
-            return res.json({
+            return res.status(404).json({
                 success: false,
                 message: "User doesn't exists!"
             })
         }
-        const isMatch = await bcrypt.compare(password, user.password)
 
+        const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch) {
-            return res.json({
+            return res.status(401).json({
                 success: false,
                 message: "Password is incorrect, try again!"
             })
         }
 
-        const token = jwt.sign({
-            id: user._id,
-            name: user.name,
-            email: user.email
-        },
+        const token = jwt.sign(
+            {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            },
             process.env.JWT_SECRET,
             {
                 expiresIn: Number(process.env.TOKEN_EXPIRE),
             }
         )
-        res.json({
+
+        res.status(200).json({
             success: true,
-            token: token,
+            token,
             message: "Login successfully!"
         })
 
     } catch (error) {
         console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
     }
 }
 
@@ -54,21 +56,21 @@ const registerUser = async (req, res) => {
 
         const existsUser = await userModel.findOne({ email })
         if (existsUser) {
-            return res.json({
+            return res.status(409).json({
                 success: false,
                 message: "User already exists!"
             })
         }
 
         if (!validator.isEmail(email)) {
-            return res.json({
+            return res.status(400).json({
                 success: false,
                 message: "Please enter a valid email!"
             })
         }
 
         if (password.length < 6) {
-            return res.json({
+            return res.status(400).json({
                 success: false,
                 message: "Please enter a strong password!"
             })
@@ -78,21 +80,21 @@ const registerUser = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt)
 
         const newUser = new userModel({
-            name: name,
-            email: email,
+            name,
+            email,
             password: hashedPassword
         })
 
-        const user = await newUser.save()
+        await newUser.save()
 
-        res.json({
+        res.status(201).json({
             success: true,
             message: "Register successfully!"
         })
 
     } catch (error) {
         console.log(error)
-        res.json({
+        res.status(500).json({
             success: false,
             message: error.message
         })
@@ -106,27 +108,26 @@ const adminLogin = async (req, res) => {
         const adminPassword = process.env.ADMIN_PASSWORD || ""
 
         if (email === adminEmail && password === adminPassword) {
-
-            const token = jwt.sign({
-                email: adminEmail
-            },
+            const token = jwt.sign(
+                { email: adminEmail },
                 process.env.JWT_SECRET,
                 {
                     expiresIn: Number(process.env.TOKEN_EXPIRE),
-                })
-            res.json({
+                }
+            )
+
+            res.status(200).json({
                 success: true,
-                token: token
+                token
             })
-        }
-        else {
-            res.json({
+        } else {
+            res.status(401).json({
                 success: false,
                 message: "Invalid email or password, try again!"
             })
         }
     } catch (error) {
-        res.json({
+        res.status(500).json({
             success: false,
             message: error.message
         })
